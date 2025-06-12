867448d8876ece38792997fe0330321650fd3ea1
RafaelEngels <rafaelengel30@gmail.com>
2025-06-12 18:41:33 -0300

    Implementaçao de páginas

---
+++ projeto_web/inicio.js
@@ -0,0 +1,19 @@
+
+//funcoes basicas pra troca de tela
+//script pra pagina inicial porem pode ser usado pra todos as paginas
+
+
+
+document.getElementById('btnalgo').onclick = function() {
+  window.location.href = 'paginaIdeiaPrincipal.html';
+};
+
+document.getElementById('btnLixoType').onclick = function() {
+  window.location.href = 'paginaTiposLixo.html';
+};
+
+document.getElementById('btnCredits').onclick = function() {
+  window.location.href = 'paginafinal.html';
+};
+
+
